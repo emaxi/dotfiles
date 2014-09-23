e38d9076447bc23c6771f402fa84bb5a028aeb6e
Maxi Gonzalez <emaxi.gonzalez@gmail.com>
2014-09-23 19:53:40 -0300

    Add favicon_generator script

---
+++ bash/favicon_generator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Converts an image in a multi-resolution favicon
+# Requires Imagemagick
+
+if [[ "$#" != "2" ]]; then
+    echo "Usage: $0 input.png output.ico" >&2
+    exit 1
+fi
+
+input="$1"
+output="$2"
+sizes="16 32 64 128 256"
+tmp_dir=$(mktemp -d /tmp/favicon.XXXXXXXXXX)
+files=""
+
+for size in $sizes; do
+    file="$tmp_dir/$size.png"
+    convert "$input" -depth 8 -background transparent -flatten -resize "${size}x${size}" "$file"
+    files="$files $file"
+done
+
+convert $files $output
+rm -R $tmp_dir
